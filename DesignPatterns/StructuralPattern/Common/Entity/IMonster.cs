2ee28a1f8e7348d388cc9a4019ed13ceb31bc35e
Dawid Pereira <pereiradawid@outlook.com>
2019-11-05 20:00:09 +0100

    Created common files

---
+++ DesignPatterns/StructuralPattern/Common/Entity/IMonster.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPatterns.StructuralPattern.Common.Entity
+{
+    public interface IMonster
+    {
+        void Attack();
+
+        void Defense();
+    }
+}
---
+++ DesignPatterns/StructuralPattern/Common/Entity/Monster.cs
@@ -0,0 +1,24 @@
+﻿using DesignPatterns.StructuralPattern.Bridge;
+
+namespace DesignPatterns.StructuralPattern.Common.Entity
+{
+    public class Monster : IMonster
+    {
+        private readonly IBehavior _behavior;
+
+        public Monster(IBehavior behavior)
+        {
+            _behavior = behavior;
+        }
+
+        public void Attack()
+        {
+            _behavior.PrimarySkill();
+        }
+
+        public void Defense()
+        {
+            _behavior.Defense();
+        }
+    }
+}
